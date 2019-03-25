e8722c34d51857825ef304643df28a1aefedab5d
Ddubby <league@WorthyMini-5.attlocal.net>
2019-03-25 16:59:01 -0700

    EncapsulateTheData

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -15,20 +15,69 @@ package _01_introduction_to_encapsulation;
  * 
  * */
 
-
 public class EncapsulateTheData {
-	//1. Encapsulate the member variables.
-	//   Add restrictions to the setters according to the comment.
-	
-	//2. Create a new JUnit Test case and write tests to verify that 
-	//   the member variables' getters and setters are working
-	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj; //can be any object type except String. Strings get turned into objects.
-	
+	// 1. Encapsulate the member variables.
+	// Add restrictions to the setters according to the comment.
+
+	// 2. Create a new JUnit Test case and write tests to verify that
+	// the member variables' getters and setters are working
+
+	private int itemsReceived; // must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; // must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; // must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj; // can be any object type except String. Strings get turned into objects.
+
 	public static void main(String[] args) {
-		
+
+	}
+
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+
+	public void setItemsReceived(int items) {
+		if (items < 0) {
+			this.itemsReceived = 0;
+		} else {
+			this.itemsReceived = items;
+		}
+	}
+
+	public float getDegrees() {
+		return degreesTurned;
+	}
+
+	public void setDegrees(int degrees) {
+		if (degrees < 0.0 || degrees > 360.0) {
+			this.degreesTurned = -1;
+		} else {
+			this.degreesTurned = degrees;
+		}
+	}
+
+	public String getClature() {
+		return nomenclature;
+	}
+
+	public void setClature(String clature) {
+		if (clature.length() == 0) {
+			this.nomenclature.equals(" ");
+		} else {
+			this.nomenclature.equals(clature);
+		}
+	}
+
+	public Object getObject() {
+		return memberObj;
+	}
+
+	public void setObject(String object) {
+		Object trueObject;
+		trueObject = object;
+		this.memberObj = trueObject;
+	}
+
+	public void setObject(Object object) {
+		this.memberObj = object;
 	}
 }
---
+++ src/_01_introduction_to_encapsulation/EncapsulateTheDataTest.java
@@ -0,0 +1,14 @@
+package _01_introduction_to_encapsulation;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EncapsulateTheDataTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
